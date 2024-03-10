64f82e12d2068bfda510bd4fd668c18eafa77cbc
JSLittleChicken <oksana.petrovich.2019@gmail.com>
2024-03-10 18:20:41 +0200

    hw45 done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React, { Component }  from "react";
+
+class Header extends Component {
+    render() {
+
+
+        return (
+                    <header className="header">
+                        <h3>Welcome</h3>
+                        
+                    </header>
+                )
+    }
+}
+ export default Header;
+
+
