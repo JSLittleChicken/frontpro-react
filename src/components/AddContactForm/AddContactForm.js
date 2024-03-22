11623787c53d6d248bc012c35135f115721f2c63
JSLittleChicken <oksana.petrovich.2019@gmail.com>
2024-03-22 15:11:21 +0200

    buttons added

--- src/components/AddContactForm/AddContactForm.js
+++ src/components/AddContactForm/AddContactForm.js
@@ -1,8 +1,11 @@
+
 import { useState } from "react";
 
 const AddContactForm = (props) => {
 
     const [formData, setFormData] = useState({ name: "", phone: "", username: "" });
+    const [isFormVisible, setIsFormVisible] = useState(false);
+    const [isButtonVisible, setIsButtonVisible] = useState(true);
 
     function handleSubmit(e) {
         e.preventDefault();
@@ -10,28 +13,40 @@ const AddContactForm = (props) => {
         const contact = Object.fromEntries(data.entries());
         props.addContact(contact);
         setFormData({ name: "", phone: "", username: "" })
+        setIsFormVisible(false);
+        setIsButtonVisible(true);
+    }
+
+    function handleCancelClick() {
+        setFormData({ name: "", phone: "", username: "" })
+        setIsFormVisible(false);
+        setIsButtonVisible(true);
     }
 
     return (
         <div>
-            <h2>Add a new contact</h2>
-            <form onSubmit={handleSubmit}>
-                <input
-                    value={formData.name}
-                    onChange={(e) => { setFormData({ ...formData, name: e.target.value }) }}
-                    type="text" name="name" required="required" placeholder="enter your name"
-                />
-                <input
-                    value={formData.username}
-                    onChange={(e) => { setFormData({ ...formData, username: e.target.value }) }}
-                    type="text" name="username" required="required" placeholder="enter your username"
-                />
-                <input
-                    value={formData.phone}
-                    onChange={(e) => { setFormData({ ...formData, phone: e.target.value }) }}
-                    type="tel" name="phone" required="required" placeholder="enter your phone number" />
-                <button type="submit">Add</button>
-            </form>
+            <button className={isButtonVisible ? '' : 'hidden'} onClick={() => {setIsButtonVisible(false); setIsFormVisible(true)}}>Add new contact</button>
+            <div className={isFormVisible ? '' : 'hidden'}>
+                <h2>Add a new contact</h2>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        value={formData.name}
+                        onChange={(e) => { setFormData({ ...formData, name: e.target.value }) }}
+                        type="text" name="name" required="required" placeholder="enter your name"
+                    />
+                    <input
+                        value={formData.username}
+                        onChange={(e) => { setFormData({ ...formData, username: e.target.value }) }}
+                        type="text" name="username" required="required" placeholder="enter your username"
+                    />
+                    <input
+                        value={formData.phone}
+                        onChange={(e) => { setFormData({ ...formData, phone: e.target.value }) }}
+                        type="tel" name="phone" required="required" placeholder="enter your phone number" />
+                    <button type="submit">Add</button>
+                    <button onClick={handleCancelClick}>Cancel</button>
+                </form>
+            </div>
         </div>
     )
 }
